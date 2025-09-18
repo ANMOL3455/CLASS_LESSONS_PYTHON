17a5f1ba4fc7a7e78a8baa0420cd9e213c47b39e
ANMOL3455 <sapkota.anmol822@gmail.com>
2025-09-18 07:33:14 +0545

    edited-2082/06/02

---
+++ complex.py
@@ -0,0 +1,13 @@
+#here we declare the complex
+complex_number_1=23+45j
+complex_number_2=25+4j
+#here the format is a+bj
+#complex numbers can be added,substracted and conducted other functions too
+print("the complex we declared is : ",complex_number_1)
+#adding the complex number
+print("the sum is:",complex_number_1+complex_number_2)
+#complex number have 2 parts i.e : imaginary and real part
+#to print the real part
+print("the real part is : ",complex_number_1.real)
+print("the imaginary part is : ",complex_number_1.imag)
+#sakyo
---
+++ type_conversion.py
@@ -0,0 +1,3 @@
+#the basic type conversion functions are:
+#int(),float(),str(),complex(),bool()
+#first lets learn about the other type to int conversion
