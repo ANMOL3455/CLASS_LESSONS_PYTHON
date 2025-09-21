ae5a93dd1ec91faab230001366848750d83a62b0
ANMOL3455 <sapkota.anmol822@gmail.com>
2025-09-21 08:53:10 +0545

    edited-2082/06/05

---
+++ fundamentals_immutability.py
@@ -0,0 +1,21 @@
+#fundamental vs mutability
+#remember that fudamentsl values are immutable
+#actually mutabiity means that can be changed or have a nature of ability of changing
+#example
+name="harikrishna"
+#now it cannot be changed like making 
+name="gaurididi"
+
+
+#what if trying to change the variable value forcely
+#in such case new object forms and new adddress is given to the variable
+#now why is string immutable ??
+#what is this happens
+name_1="hari"
+name_2="hari"
+#and in such way 10 other boys have name hari in a institution now all variables wil be refering the same object
+#and if i wanna change name_2 to ram then name_1 will also be ram than that makes conflicts so fundaments datas are
+#immutable
+#same thing happents in int,float,bool,complex etc
+
+
