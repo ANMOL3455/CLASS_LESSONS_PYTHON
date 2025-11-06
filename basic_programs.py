a35f6bcbbd1bc39951025f83031e560e6c438f30
ANMOL3455 <sapkota.anmol822@gmail.com>
2025-11-06 10:45:10 +0545

    2082/07/20

---
+++ basic_programs.py
@@ -0,0 +1,20 @@
+#area of rectangle
+
+#area of square
+
+#area of circle
+
+#volume of rectangle
+
+#volume of square
+
+
+#perimetre of Square
+length_square=eval(input("enter the length: "))
+print(f"The perimeter of square is: {4*length_square}")
+
+#Simple Intrest
+time=int(input("enter the time: "))
+principle=int(input("enter the principle: "))
+rate=int(input("enter the rate: "))
+print(f"the si is: {(principle*time*rate)/100}")
---
+++ operator_presedence.py
@@ -0,0 +1,3 @@
+#operator having highest presedence operates first
+#presedence order
+print(eval("3/5+6+(3*2)**2//3-20+15"))
