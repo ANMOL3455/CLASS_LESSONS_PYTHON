6fec325dbad01fe6a61c33c857bc505fc3e406eb
ANMOL3455 <sapkota.anmol822@gmail.com>
2025-11-10 08:43:08 +0545

    edited-2082/07/24

---
+++ nested_if.py
@@ -0,0 +1,17 @@
+#greatest of three
+number1=int(input("enter the number"))
+number2=int(input("enter the number"))
+number3=int(input("enter the number"))
+if number1>number2:
+          if number1>number3:
+                 print(f"{number1 } is greatest")
+          else:
+              print(F"{number3} is greatest")
+else:
+    if number2>number3:
+        print(f"{number2} is gratest")
+    else:
+        print(f"{number3} is greatest")
+              
+
+    
